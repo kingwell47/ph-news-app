b23d97be3752636ecf4928cd8de9a232d35e6b04
kingwell47 <joeldoctor47@gmail.com>
2021-11-24 18:21:47 +0800

    changed folder structure

--- server.js
+++ server.js
@@ -26,10 +26,10 @@ app.get("/api/articles/:modifier", async (req, res) => {
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static("/build"));
+  app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
